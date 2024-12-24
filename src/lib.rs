39a5d6ff86e61f8308c37ff38ad0ad0cbc0c0516
cksac <cs.cksac@gmail.com>
2024-12-24 23:46:25 +0800

    update deps

--- src/lib.rs
+++ src/lib.rs
@@ -6,4 +6,7 @@ mod taffy_tree;
 pub use taffy_tree::{TaffyTree, TaffyTreeChildIter, TaffyTreeView};
 
 mod layout;
+pub use compose_rt;
 pub use layout::{LayoutError, LayoutResult, TaffyLayout};
+/// re-exports
+pub use taffy;
